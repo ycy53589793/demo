1d645bf84729f5a3fd03723b8cde61102d24da96
vip <vip_y@foxmail.com>
2014-08-08 10:39:47 +0800

    增加userAction类

---
+++ myproject/src/com/user/action/UserAction.java
@@ -0,0 +1,12 @@
+package com.user.action;
+
+import com.BaseAction;
+
+public class UserAction extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5397058623582673843L;
+
+}
