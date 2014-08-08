5c87ac18b196f04724345a2e3be85083ffb32385
vip <vip_y@foxmail.com>
2014-08-08 11:46:14 +0800

    增加类注释

--- myproject/src/com/common/ErrorAction.java
+++ myproject/src/com/common/ErrorAction.java
@@ -2,6 +2,16 @@ package com.common;
 
 import com.BaseAction;
 
+/**
+ * Description:   登陆出错页面action
+ * @author: 杨聪艺
+ * Create Date: 2014-8-8
+ * <pre>
+ * 修改记录:
+ * 修改后版本			修改人		修改日期			修改内容 
+ * 2014-8-8.1		杨聪艺		2014-8-8		create					
+ * </pre>
+ */
 public class ErrorAction extends BaseAction {
 
 	/**
--- myproject/src/com/common/LoginAction.java
+++ myproject/src/com/common/LoginAction.java
@@ -5,6 +5,16 @@ import com.opensymphony.xwork2.Action;
 import com.user.service.UserService;
 import com.util.ResultMessage;
 
+/**
+ * Description:   登陆页面action
+ * @author: 杨聪艺
+ * Create Date: 2014-8-8
+ * <pre>
+ * 修改记录:
+ * 修改后版本			修改人		修改日期			修改内容 
+ * 2014-8-8.1		杨聪艺		2014-8-8		create					
+ * </pre>
+ */
 public class LoginAction extends BaseAction {
 
 	/**
