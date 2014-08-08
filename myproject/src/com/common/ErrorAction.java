3de0e4e1171de0538f6fb4992a558f7c9889d7fb
vip <vip_y@foxmail.com>
2014-08-08 11:44:08 +0800

    新增errorAction

---
+++ myproject/src/com/common/ErrorAction.java
@@ -0,0 +1,12 @@
+package com.common;
+
+import com.BaseAction;
+
+public class ErrorAction extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 140628443213802136L;
+
+}
