2538d8a111a875197fe41680680e77495134a512
vip <vip_y@foxmail.com>
2014-08-08 10:35:14 +0800

    增加common目录

---
+++ myproject/src/com/common/LoginAction.java
@@ -0,0 +1,53 @@
+package com.common;
+
+import com.BaseAction;
+import com.opensymphony.xwork2.Action;
+import com.user.service.UserService;
+import com.util.ResultMessage;
+
+public class LoginAction extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4641585993706926509L;
+	
+	private String username;
+	private String password;
+	
+	/**
+	 * Description :登陆时对用户进行校验
+	 * @param condition
+	 * @return
+	 * @Author: 杨聪艺
+	 * @Create Date: 2014-4-3
+	 */
+	public String checkUser() throws Exception {
+		
+		//spring自动注入
+		UserService userService = (UserService)getBean("userService");
+		
+		ResultMessage msg=userService.checkUserExist(username, password);
+		
+		if(!msg.isSuccess()) {
+			return Action.ERROR;
+		}
+		
+		return Action.SUCCESS;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
